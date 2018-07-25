d0af2211478e4a19ea2591c0ccff474aaea612fb
nalomysouza <nalomysouza@gmail.com>
2018-07-24 21:21:55 -0300

    logando com Oauth2 Basic no properties

--- src/main/java/com/example/moneyapi/MoneyApiApplication.java
+++ src/main/java/com/example/moneyapi/MoneyApiApplication.java
@@ -2,8 +2,9 @@ package com.example.moneyapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class MoneyApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/moneyapi/config/SecurityConfig.java
+++ src/main/java/com/example/moneyapi/config/SecurityConfig.java
@@ -1,22 +1,19 @@
 package com.example.moneyapi.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		auth.inMemoryAuthentication().withUser("admin").password(encoder.encode("admin")).roles("ADMIN");
-	}
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//		auth.inMemoryAuthentication().withUser("admin").password(encoder.encode("admin")).roles("ADMIN");
+//	}
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
